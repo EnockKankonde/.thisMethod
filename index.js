d48d8c26c94176e0f4b4209751d743ae7d0e2979
Enock_Kankonde <144600321+EnockKankonde@users.noreply.github.com>
2024-12-30 19:08:08 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+function Character(name){
+    this.name = name
+    this.collectedItemsArr = []
+    this.addItem = function(item){
+        this.collectedItemsArr.push(item)
+        console.log(`${this.name} now has ${this.collectedItemsArr}`)
+    }
+}
+
+const newItem = new Character('Enock')
+newItem.addItem('wand, map, potion')
+
+/*
+Challenge:
+1. Create a constructor function called 'Character'.
+2. Give it 'name' and 'collectedItemsArr' properties. 
+    - 'name' should hold the character’s name.
+	- 'collectedItemsArr' should hold an array of items. 
+       Initialise it to an empty array.
+3. Create an 'addItem' method which takes in an item as 
+   a parameter and adds it to 'collectedItemsArr'.
+4. The addItem method should log out a sentence like 
+   `Merlin now has: wand, map, potion`.
+5. Check it’s working by creating several instances of 
+   Character and adding items to their arrays.
+*/
